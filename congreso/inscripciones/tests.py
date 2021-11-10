2760c5d355136df8f413483df1fb3388b7b39eaf
Roberto <rbarrero@posteo.net>
2021-11-10 18:56:24 +0100

    refactor

--- congreso/inscripciones/tests.py
+++ congreso/inscripciones/tests.py
@@ -2,12 +2,13 @@ import json
 import factory
 import pytest
 from django.test import Client
+from django.urls import reverse
 from inscripciones.models import Inscripcion
 from inscripciones.factory import InscripcionFactory
 from mailapp.models import Plantilla
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def create_mail_template():
     Plantilla.objects.create(
         nombre="registro ok",
@@ -19,12 +20,12 @@ def create_mail_template():
 
 
 @pytest.mark.django_db
-def test_new_sign_up(create_mail_template):
+def test_new_sign_up():
     browser = Client()
     inscripcion = factory.build(dict, FACTORY_CLASS=InscripcionFactory)
     inscripcion["email2"] = inscripcion["email"]
     response = browser.post(
-        "/api/v1/form/", json.dumps(inscripcion), content_type="application/json"
+        reverse("signup"), json.dumps(inscripcion), content_type="application/json"
     )
     assert response.status_code == 200
     assert response.content == b'{"status": "ok"}'
