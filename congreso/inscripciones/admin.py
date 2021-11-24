f0b8ac3b52fa7badaabc7e94417ea81d30723461
Roberto <rbarrero@posteo.net>
2021-11-24 10:22:21 +0100

    Añadimos acción para enviar correos a los inscritos

--- congreso/inscripciones/admin.py
+++ congreso/inscripciones/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
+from excel_response import ExcelResponse
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from .models import Inscripcion
 from mailapp.utils import sendCustomMail
 from mailapp.models import Plantilla
 
 
 class InscripcionAdmin(admin.ModelAdmin):
-    actions = [
-        "confirmar_y_concluir",
-    ]
+    actions = ["confirmar_y_concluir", "exportar", "mail_seleccionados"]
     list_display = (
         "nombre",
         "apellidos",
@@ -40,6 +41,52 @@ class InscripcionAdmin(admin.ModelAdmin):
     confirmar_y_concluir.short_description = (
         "Confirmar, notificar y concluir inscripciones"
     )
+
+    def exportar(self, request, queryset):
+        data = [
+            [
+                "Nombre",
+                "Apellidos",
+                "Email",
+                "Telf",
+                "Localidad",
+                "Edad",
+                "dni",
+                "Estado inscripción",
+                "Concluído" "Fecha de inscripción",
+            ]
+        ]
+        for inscripcion in queryset:
+            concluido = "Sí" if inscripcion.terminada else "No"
+            data.append(
+                [
+                    inscripcion.nombre,
+                    inscripcion.apellidos,
+                    inscripcion.email,
+                    inscripcion.telefono,
+                    inscripcion.localidad,
+                    inscripcion.edad,
+                    inscripcion.dni,
+                    inscripcion.get_estado_inscripcion_display(),
+                    concluido,
+                    inscripcion.created_at,
+                ]
+            )
+        return ExcelResponse(data)
+        exportar.short_description = "Exportar inscripciones"
+
+    def mail_seleccionados(self, request, queryset):
+        selected = queryset.values_list("pk", flat=True)
+        ct = ContentType.objects.get_for_model(queryset.model)
+        return HttpResponseRedirect(
+            "/sendmail/?ct=%s&ids=%s"
+            % (
+                ct.pk,
+                ",".join(str(pk) for pk in selected),
+            )
+        )
+
+    mail_seleccionados.short_description = "Enviar correo a selección"
 
 
 admin.site.register(Inscripcion, InscripcionAdmin)
